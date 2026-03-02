d98af45e064ceb1532883627a69a00eaac72585a
BrahamjeetRandhawa <152489943+Braham3030@users.noreply.github.com>
2026-03-02 11:18:58 +0100

    elfproef working

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("Hello, World! This is a simple JavaScript file.");
+
+const form = document.querySelector("form");
+const input = form.querySelectorAll("input[type='text']");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+
+})
+
+
+// Elfproef
+
+const BSNnumberInput = document.querySelector(".BSNnumberInput");
+
+BSNnumberInput.addEventListener("input", () => {
+    const BSNnumber = BSNnumberInput.value;
+
+    if (BSNnumber.length === 9) {
+        BSNnumber.split("")
+        let sum = BSNnumber[0] * 9 + BSNnumber[1] * 8 + BSNnumber[2] * 7 + BSNnumber[3] * 6 + BSNnumber[4] * 5 + BSNnumber[5] * 4 + BSNnumber[6] * 3 + BSNnumber[7] * 2 - BSNnumber[8] * -1;
+        
+        total = sum / 11;
+        console.log(BSNnumber[0]);
+        console.log(sum);
+
+        console.log(total);
+        
+        if (total % 1 === 0) {
+            console.log("Valid BSN number");
+        } else {
+            console.log("Invalid BSN number");
+        }
+
+    }
+})
